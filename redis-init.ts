275669a901f66e3023a99cf6b7467718b95553a5
Hairum Qureshi <hqureshi@udel.edu>
2025-11-05 20:00:48 -0500

    package files updated for latest package installations/removals

--- redis-init.ts
+++ redis-init.ts
@@ -5,7 +5,7 @@ import chalk from "chalk";
 dotenv.config();
 
 const redis = Redis.createClient({
-	url: process.env.REDIS_URL // looks like this: redis://localhost:REDIS_PORT_NUMBER
+	url: process.env.REDIS_URL // looks like this: redis://localhost:REDIS_PORT_NUMBER,
 });
 
 redis.on("error", err => {
@@ -14,6 +14,9 @@ redis.on("error", err => {
 
 const startRedis = (async () => {
 	await redis.connect();
+
+	// Note: this line is only if you want to use a separate Redis DB
+	await redis.select(1); // connects to DB 1 (run: 'SELECT 1' in Redis-CLI to switch to DB 1)
 	console.log(
 		chalk.yellowBright("Successfully connected to"),
 		chalk.redBright("Redis")
